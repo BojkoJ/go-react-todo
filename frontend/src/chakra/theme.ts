77a88407e64f9d5eb6e8ec5f4454ed897c9000c8
BojkoJ <honzabojko@seznam.cz>
2024-07-08 11:53:16 +0200

    theme

--- frontend/src/chakra/theme.ts
+++ frontend/src/chakra/theme.ts
@@ -2,20 +2,20 @@ import { extendTheme, Theme, type ThemeConfig } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 
 const config: ThemeConfig = {
-	initialColorMode: "dark",
-	useSystemColorMode: true,
+    initialColorMode: "dark",
+    useSystemColorMode: true,
 };
 
 // 3. extend the theme
 const theme = extendTheme({
-	config,
-	styles: {
-		global: (props: Theme) => ({
-			body: {
-				backgroundColor: mode("gray.500", "")(props),
-			},
-		}),
-	},
+    config,
+    styles: {
+        global: (props: Theme) => ({
+            body: {
+                backgroundColor: mode("gray.600", "")(props),
+            },
+        }),
+    },
 });
 
 export default theme;
