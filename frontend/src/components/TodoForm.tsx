f6176c801f3c0a9d2213de10e6b03220a7a600e4
BojkoJ <honzabojko@seznam.cz>
2024-07-08 11:50:41 +0200

    Rest functionalities.

--- frontend/src/components/TodoForm.tsx
+++ frontend/src/components/TodoForm.tsx
@@ -1,36 +1,73 @@
 import { Button, Flex, Input, Spinner } from "@chakra-ui/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
+import { BASE_URL } from "../App";
 
 const TodoForm = () => {
-	const [newTodo, setNewTodo] = useState("");
-	const [isPending, setIsPending] = useState(false);
+    const [newTodo, setNewTodo] = useState("");
 
-	const createTodo = async (e: React.FormEvent) => {
-		e.preventDefault();
-		alert("Todo added!");
-	};
+    const queryClient = useQueryClient();
 
-	return (
-		<form onSubmit={createTodo}>
-			<Flex gap={2}>
-				<Input
-					type="text"
-					value={newTodo}
-					onChange={(e) => setNewTodo(e.target.value)}
-					ref={(input) => input && input.focus()}
-				/>
-				<Button
-					mx={2}
-					type="submit"
-					_active={{
-						transform: "scale(.97)",
-					}}
-				>
-					{isPending ? <Spinner size={"xs"} /> : <IoMdAdd size={30} />}
-				</Button>
-			</Flex>
-		</form>
-	);
+    const { mutate: createTodo, isPending: isCreating } = useMutation({
+        mutationKey: ["createTodo"],
+        mutationFn: async (e: React.FormEvent) => {
+            e.preventDefault();
+
+            try {
+                const res = await fetch(BASE_URL + `/todos`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ body: newTodo }),
+                });
+
+                const data = await res.json();
+
+                if (!res.ok) {
+                    throw new Error(data.error || "Something went wrong");
+                }
+
+                setNewTodo("")
+
+                return data
+            } catch (err: unknown | string | undefined) {
+                throw new Error(err as string | undefined);
+            }
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["todos"] });
+        },
+        onError: (err: Error) => {
+            alert(err.message);
+        },
+    });
+
+    return (
+        <form onSubmit={createTodo}>
+            <Flex gap={2}>
+                <Input
+                    type="text"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    ref={(input) => input && input.focus()}
+                />
+                <Button
+                    mx={2}
+                    type="submit"
+                    _active={{
+                        transform: "scale(.97)",
+                    }}
+                >
+                    {isCreating ? (
+                        <Spinner size={"xs"} />
+                    ) : (
+                        <IoMdAdd size={30} />
+                    )}
+                </Button>
+            </Flex>
+        </form>
+    );
 };
 export default TodoForm;
